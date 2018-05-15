185800efde4d335e7dd2b5482f37ac49eae8f6a0
Cole Hart <cole@colehart.com>
2018-05-15 15:52:02 -0600

    cradded create address feature test

---
+++ spec/features/addresses/user_can_create_new_address_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe 'when user visits student page' do
+  scenario 'a user can create new address by filling out a form' do
+    student = Student.create!(name: 'Alan Turing')
+
+    visit student_path(student)
+    
+    description = 'home'
+    street = '805 Turing Blvd'
+    city = 'London'
+    state = 'England'
+    zip_code = '80026'
+
+    fill_in 'address[description]', with: description
+    fill_in 'address[street]', with: street
+    fill_in 'address[city]', with: city
+    fill_in 'address[state]', with: state
+    fill_in 'address[zip_code]', with: zip_code
+
+    click_on 'Create Address'
+
+    expect(current_path).to eq(student_path(student))
+
+    expect(page).to have_content(description)
+    expect(page).to have_content(street)
+    expect(page).to have_content(city)
+    expect(page).to have_content(state)
+    expect(page).to have_content(zip_code)
+  end
+end
