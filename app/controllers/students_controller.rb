de44bb93088e5da95c4394a47136e9bfdc65411b
Cole Hart <cole@colehart.com>
2018-05-15 14:55:15 -0600

    passing create feature test

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,6 +13,15 @@ class StudentsController < ApplicationController
   end
 
   def create
+    @student = Student.new(student_params)
+    @student.save
 
+    redirect_to student_path(@student)
+  end
+
+  private
+
+  def student_params
+    params.require(:student).permit(:name)
   end
 end
