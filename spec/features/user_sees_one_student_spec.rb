bbdfdce01a23e3242cb1ca1d08460c8e8c0e5921
Cole Hart <cole@colehart.com>
2018-05-15 13:27:14 -0600

    first feature and model tests failing

--- spec/features/user_sees_one_student_spec.rb
+++ spec/features/user_sees_one_student_spec.rb
@@ -1,2 +1,10 @@
 # spec/features/ student :show test
 require 'rails_helper'
+
+describe 'user sees one article' do
+  it 'shows one student' do
+    student1 = Student.create!(name: 'Cole Hart')
+
+    expect(page).to have_content(student1.name)
+  end
+end
---
+++ spec/models/student_spec.rb
@@ -0,0 +1,8 @@
+# spec/models/student
+require 'rails_helper'
+
+describe Student, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+  end
+end
