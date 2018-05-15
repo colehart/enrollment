16c835985285dc4f5e87c6696d40af858a234850
Cole Hart <cole@colehart.com>
2018-05-15 14:21:12 -0600

    added navigation links tests to feature tests

--- spec/features/user_sees_all_students_spec.rb
+++ spec/features/user_sees_all_students_spec.rb
@@ -3,17 +3,19 @@ require 'rails_helper'
 
 describe 'user sees list of students' do
   context 'they visit /students' do
-    it 'shows all students' do
+    it 'shows all students and navigation' do
       student1 = Student.create!(name: 'Cole Hart')
       student2 = Student.create!(name: 'Manoj Panta')
       student3 = Student.create!(name: 'Tyler Westlie')
-      new_student_button = 'Create a New Student'
+      student_index_button = 'All Students'
+      new_student_button = 'Create New Student'
 
       visit students_path
-      save_and_open_page
+      
       expect(page).to have_link(student1.name)
       expect(page).to have_link(student2.name)
       expect(page).to have_link(student3.name)
+      expect(page).to have_link(student_index_button)
       expect(page).to have_link(new_student_button)
     end
   end
--- spec/features/user_sees_one_student_spec.rb
+++ spec/features/user_sees_one_student_spec.rb
@@ -2,11 +2,15 @@
 require 'rails_helper'
 
 describe 'user sees one student' do
-  it 'shows one student' do
+  it 'shows one student and navigation' do
     student1 = Student.create!(name: 'Cole Hart')
+    student_index_button = 'All Students'
+    new_student_button = 'Create New Student'
 
     visit student_path(student1)
 
     expect(page).to have_content(student1.name)
+    expect(page).to have_link(student_index_button)
+    expect(page).to have_link(new_student_button)
   end
 end
