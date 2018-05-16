8cdc0b892897588cd54547890f108626694ca8f7
Cole Hart <cole@colehart.com>
2018-05-16 13:46:28 -0600

    created routes and first migration to create courses table with name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_15_224210) do
+ActiveRecord::Schema.define(version: 2018_05_16_185931) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,10 @@ ActiveRecord::Schema.define(version: 2018_05_15_224210) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "student_id"
+  end
+
+  create_table "courses", force: :cascade do |t|
+    t.string "name"
   end
 
   create_table "students", force: :cascade do |t|
